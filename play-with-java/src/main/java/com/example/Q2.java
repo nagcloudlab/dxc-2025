5db3cb0f4df12f8a6d7e7f6e5a093e7bd0338409
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2025-01-06 19:47:37 +0530

    session1 qs

---
+++ play-with-java/src/main/java/com/example/Q2.java
@@ -0,0 +1,102 @@
+package com.example;
+
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.InvocationTargetException;
+
+// container ( infra code ) & component ( business code ) style
+
+//---------------------------------
+// auth.jar
+//---------------------------------
+/**
+ * team 1
+ */
+
+// e.g Servlet
+class WebComponent{
+    @RequestMapping(url = "/dxc/login")
+    public void handleLoginRequest(String userName,String password){
+        //...
+        System.out.println("Login request processed");
+    }
+
+    @RequestMapping(url = "/dxc/register")
+    public void handleRegisterRequest(String userName,String password,String name){
+        //...
+        System.out.println("Register request processed");
+    }
+}
+
+//---------------------------------
+
+
+
+/**
+ * third party team
+ */
+
+@Target({java.lang.annotation.ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@interface RequestMapping{
+    String url();
+}
+
+class WebContainer{
+    public void processRequest(String reqUrl){
+        // create / use thread pool to process the request
+        // parsing req body
+        // validation....
+        // processing request..
+
+        try {
+            // dynamic class/jar loading
+            //...
+            Class<?> clazz=Class.forName("com.example.WebComponent"); // dynamic class loading
+
+            // Reflection Api
+            Object obj=clazz.newInstance();
+
+            // get all methods
+            java.lang.reflect.Method[] methods=clazz.getMethods();
+
+            for(java.lang.reflect.Method method:methods){
+                RequestMapping requestMapping=method.getAnnotation(RequestMapping.class);
+                if(requestMapping!=null){
+                    if(requestMapping.url().equals(reqUrl)){
+                        // invoke the method
+                        if(requestMapping.url().equals("/dxc/login"))
+                            method.invoke(obj,new Object[]{"admin","admin"});
+                        else if(requestMapping.url().equals("/dxc/register"))
+                        method.invoke(obj,new Object[]{"admin","admin","DXC"});
+                    }
+                }
+            }
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
+
+
+
+
+
+public class Q2 {
+    public static void main(String[] args) {
+
+        WebContainer container=new WebContainer(); // e.g Tomcat
+        container.processRequest("/dxc/login");
+        container.processRequest("/dxc/register");
+
+    }
+}
